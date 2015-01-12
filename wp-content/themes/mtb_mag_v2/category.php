be7611fb57bd3ad6c318a86eeecb33310fb5be8d
gordon peterson <gordon.peterson@imail.org>
2015-01-12 02:45:14 -0700

    got the ad space being generated in the category page

--- wp-content/themes/mtb_mag_v2/category.php
+++ wp-content/themes/mtb_mag_v2/category.php
@@ -30,16 +30,49 @@ get_header(); ?>
 
 			<?php
 					// Start the Loop.
+					$count = 0;
+					$increment = 0;
+					$adArray = array(3,4,5,11);
+					// $blah = $adArray[0];
+
+					// var_dump( $adArray );
+					echo "<hr>";
+					// var_dump( $adArray[0] );
+					// echo "$blah";
+					echo "<hr>";
+
 					while ( have_posts() ) : the_post();
+						$count++;
+
+						// $blah = count( $adArray );
+						// var_dump( $blah );
+
+						if (array_key_exists($increment, $adArray)) {
+							$currentIndex = $adArray[$increment];
+						}
+						
+
+						if( $count == $currentIndex ){
+
+							if( $currentIndex == 5 ){
+								echo "<div class='ad ad2'>$adArray[$increment]</div>";
+							} elseif ($currentIndex == 11) {
+								echo "<div class='ad ad3'>$adArray[$increment]</div>";
+							} else {
+								echo "<div class='ad'>$adArray[$increment]</div>";
+							}
 
 
-					/*
-					 * Include the post format-specific template for the content. If you want to
-					 * use this in a child theme, then include a file called called content-___.php
-					 * (where ___ is the post format) and that will be used instead.
-					 */
-					// get_template_part( 'content', get_post_format() );
-					get_template_part( 'content', 'small' ); //....this gets the content-small.php template part
+							$increment++;
+						}
+
+						/*
+						 * Include the post format-specific template for the content. If you want to
+						 * use this in a child theme, then include a file called called content-___.php
+						 * (where ___ is the post format) and that will be used instead.
+						 */
+						// get_template_part( 'content', get_post_format() );
+						get_template_part( 'content', 'small' ); //....this gets the content-small.php template part
 
 					endwhile;
 					// Previous/next page navigation.
