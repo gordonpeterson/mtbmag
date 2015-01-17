3df3718753cc4c79b65bbf3326a605444df31140
gordon peterson <gordon.peterson@imail.org>
2015-01-17 10:29:49 -0700

    removed the sidebar

--- wp-content/themes/mtb_mag_v2/footer.php
+++ wp-content/themes/mtb_mag_v2/footer.php
@@ -14,7 +14,6 @@
 
 		<footer id="colophon" class="site-footer" role="contentinfo">
 
-			<?php get_sidebar( 'footer' ); ?>
 
 			<div class="site-info">
 				<?php do_action( 'twentyfourteen_credits' ); ?>
