ea981d928a1c5bc9107024de825b6a86fbc8afd5
gordon peterson <gordon.peterson@imail.org>
2015-01-17 16:37:41 -0700

    removed sidebars and widgets

--- wp-content/themes/twentyfourteen/functions.php
+++ wp-content/themes/twentyfourteen/functions.php
@@ -81,7 +81,7 @@ function twentyfourteen_setup() {
 	// This theme uses wp_nav_menu() in two locations.
 	register_nav_menus( array(
 		'primary'   => __( 'Top primary menu', 'twentyfourteen' ),
-		'secondary' => __( 'Secondary menu in left sidebar', 'twentyfourteen' ),
+		// 'secondary' => __( 'Secondary menu in left sidebar', 'twentyfourteen' ),
 	) );
 
 	/*
@@ -166,34 +166,34 @@ function twentyfourteen_has_featured_posts() {
 function twentyfourteen_widgets_init() {
 	require get_template_directory() . '/inc/widgets.php';
 	register_widget( 'Twenty_Fourteen_Ephemera_Widget' );
-
-	register_sidebar( array(
-		'name'          => __( 'Primary Sidebar', 'twentyfourteen' ),
-		'id'            => 'sidebar-1',
-		'description'   => __( 'Main sidebar that appears on the left.', 'twentyfourteen' ),
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-	) );
-	register_sidebar( array(
-		'name'          => __( 'Content Sidebar', 'twentyfourteen' ),
-		'id'            => 'sidebar-2',
-		'description'   => __( 'Additional sidebar that appears on the right.', 'twentyfourteen' ),
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-	) );
-	register_sidebar( array(
-		'name'          => __( 'Footer Widget Area', 'twentyfourteen' ),
-		'id'            => 'sidebar-3',
-		'description'   => __( 'Appears in the footer section of the site.', 'twentyfourteen' ),
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-	) );
+	
+	// register_sidebar( array(
+	// 	'name'          => __( 'Primary Sidebar', 'twentyfourteen' ),
+	// 	'id'            => 'sidebar-1',
+	// 	'description'   => __( 'Main sidebar that appears on the left.', 'twentyfourteen' ),
+	// 	'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+	// 	'after_widget'  => '</aside>',
+	// 	'before_title'  => '<h1 class="widget-title">',
+	// 	'after_title'   => '</h1>',
+	// ) );
+	// register_sidebar( array(
+	// 	'name'          => __( 'Content Sidebar', 'twentyfourteen' ),
+	// 	'id'            => 'sidebar-2',
+	// 	'description'   => __( 'Additional sidebar that appears on the right.', 'twentyfourteen' ),
+	// 	'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+	// 	'after_widget'  => '</aside>',
+	// 	'before_title'  => '<h1 class="widget-title">',
+	// 	'after_title'   => '</h1>',
+	// ) );
+	// register_sidebar( array(
+	// 	'name'          => __( 'Footer Widget Area', 'twentyfourteen' ),
+	// 	'id'            => 'sidebar-3',
+	// 	'description'   => __( 'Appears in the footer section of the site.', 'twentyfourteen' ),
+	// 	'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+	// 	'after_widget'  => '</aside>',
+	// 	'before_title'  => '<h1 class="widget-title">',
+	// 	'after_title'   => '</h1>',
+	// ) );
 }
 add_action( 'widgets_init', 'twentyfourteen_widgets_init' );
 
