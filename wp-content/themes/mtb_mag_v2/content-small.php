6b1dafa6cdf8ac67200b9b5fd8a9fb079e73cdad
gordon peterson <gordon.peterson@imail.org>
2015-01-17 21:32:09 -0700

    added .post-thumbnail class to the content-small page. cleaned up mobile styles

--- wp-content/themes/mtb_mag_v2/content-small.php
+++ wp-content/themes/mtb_mag_v2/content-small.php
@@ -13,7 +13,9 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('small'); ?>>
 	
 	<?php //twentyfourteen_post_thumbnail(); ?>
-	<?php the_post_thumbnail('wt780_450'); ?>
+	<div class="post-thumbnail">
+		<?php the_post_thumbnail('wt780_450'); ?>
+	</div>
 
 	<div class="post-info">
 		<header class="entry-header">
