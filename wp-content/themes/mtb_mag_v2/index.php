b80c1c2e34c69a3ea274193fdd3ac25828fa95ee
gordon peterson <gordon.peterson@imail.org>
2015-01-17 23:40:38 -0700

    got the header styled responsive

--- wp-content/themes/mtb_mag_v2/index.php
+++ wp-content/themes/mtb_mag_v2/index.php
@@ -14,7 +14,7 @@
  * @since Twenty Fourteen 1.0
  */
 
-get_header(); ?>
+get_header('gordon'); ?>
 
 <div id="main-content" class="main-content">
 
--- wp-content/themes/mtb_mag_v2/single.php
+++ wp-content/themes/mtb_mag_v2/single.php
@@ -7,7 +7,7 @@
  * @since Twenty Fourteen 1.0
  */
 
-get_header(); ?>
+get_header('gordon'); ?>
 
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
