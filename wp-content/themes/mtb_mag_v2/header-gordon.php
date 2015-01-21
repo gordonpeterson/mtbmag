3e6ba95d0d0a9a82b985958c18d28899bd74e349
gordon peterson <gordon.peterson@imail.org>
2015-01-21 06:57:18 -0700

    fixed the header

--- wp-content/themes/mtb_mag_v2/header-gordon.php
+++ wp-content/themes/mtb_mag_v2/header-gordon.php
@@ -104,6 +104,12 @@
 			<nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
 				<button class="menu-toggle"><?php _e( 'Primary Menu', 'twentyfourteen' ); ?></button>
 				<a class="screen-reader-text skip-link" href="#content"><?php _e( 'Skip to content', 'twentyfourteen' ); ?></a>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+			</nav>
+		<!-- 
+			<nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
+				<button class="menu-toggle"><?php _e( 'Primary Menu', 'twentyfourteen' ); ?></button>
+				<a class="screen-reader-text skip-link" href="#content"><?php _e( 'Skip to content', 'twentyfourteen' ); ?></a>
 				<? //php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
 
 				<div class="menu-inglese-container">
@@ -117,6 +123,7 @@
 					</ul>
 				</div>
 			</nav>
+		-->
 		</div>
 
 		<div id="search-container" class="search-box-wrapper hide">
