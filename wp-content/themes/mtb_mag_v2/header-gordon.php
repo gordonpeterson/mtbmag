095204125edf8718bb1a2d05fc6d1b527ba2ad1e
gordon peterson <gordon.peterson@imail.org>
2015-01-24 23:27:53 -0700

    pulled google ad content into header

--- wp-content/themes/mtb_mag_v2/header-gordon.php
+++ wp-content/themes/mtb_mag_v2/header-gordon.php
@@ -65,6 +65,76 @@
 </head>
 
 <body <?php body_class(); ?> ng-app="mtbMagApp">
+
+<!-- start -->
+			<?php 
+				define("ICL_LANGUAGE_CODE", "en");
+			 ?>
+			
+			<?php	if(ICL_LANGUAGE_CODE == 'en'){ ?>
+				<script type='text/javascript'>
+				var googletag = googletag || {};
+				googletag.cmd = googletag.cmd || [];
+				(function() {
+				var gads = document.createElement('script');
+				gads.async = true;
+				gads.type = 'text/javascript';
+				var useSSL = 'https:' == document.location.protocol;
+				gads.src = (useSSL ? 'https:' : 'http:') + 
+				'//www.googletagservices.com/tag/js/gpt.js';
+				var node = document.getElementsByTagName('script')[0];
+				node.parentNode.insertBefore(gads, node);
+				})();
+				</script>
+
+				<script type='text/javascript'>
+				googletag.cmd.push(function() {
+				googletag.defineSlot('/1031065/MTBMAG-EN-Header', [[728, 90], [950, 150]], 'div-gpt-ad-1402004698859-0').addService(googletag.pubads());
+				googletag.pubads().enableSingleRequest();
+				googletag.enableServices();
+				});
+				</script>
+				<!-- MTBMAG-EN-Header -->
+				<div id='div-gpt-ad-1402004698859-0'>
+				<script type='text/javascript'>
+				googletag.cmd.push(function() { googletag.display('div-gpt-ad-1402004698859-0'); });
+				</script>
+				</div>
+			<?php	}
+				else {?>
+					<script type='text/javascript'>
+					var googletag = googletag || {};
+					googletag.cmd = googletag.cmd || [];
+					(function() {
+					var gads = document.createElement('script');
+					gads.async = true;
+					gads.type = 'text/javascript';
+					var useSSL = 'https:' == document.location.protocol;
+					gads.src = (useSSL ? 'https:' : 'http:') + 
+					'//www.googletagservices.com/tag/js/gpt.js';
+					var node = document.getElementsByTagName('script')[0];
+					node.parentNode.insertBefore(gads, node);
+					})();
+					</script>
+
+					<script type='text/javascript'>
+					googletag.cmd.push(function() {
+					googletag.defineSlot('/1031065/MTBMAG-Header-home', [[728, 90], [950, 150]], 'div-gpt-ad-1402004398340-0').addService(googletag.pubads());
+					googletag.pubads().enableSingleRequest();
+					googletag.enableServices();
+					});
+					</script>
+					<!-- MTBMAG-Header-home -->
+					<div id='div-gpt-ad-1402004398340-0'>
+					<script type='text/javascript'>
+					googletag.cmd.push(function() { googletag.display('div-gpt-ad-1402004398340-0'); });
+					</script>
+					</div>
+				
+			<?php } ?>
+
+			<!-- end -->
+
 <div id="page" class="hfeed site">
 	<?php if ( get_header_image() ) : ?>
 	<div id="site-header">
