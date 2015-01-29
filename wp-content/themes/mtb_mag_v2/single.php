690bd922ec66cfeea4907b1a6699e79a29b395cc
gordon peterson <gordon.peterson@imail.org>
2015-01-28 20:37:00 -0700

    updated the fixed positions for the main nav and wordpress nav

--- wp-content/themes/mtb_mag_v2/single.php
+++ wp-content/themes/mtb_mag_v2/single.php
@@ -7,7 +7,7 @@
  * @since Twenty Fourteen 1.0
  */
 
-get_header(); ?>
+get_header('gordon'); ?>
 
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
@@ -35,6 +35,6 @@ get_header(); ?>
 	</div><!-- #primary -->
 
 <?php
-get_sidebar( 'content' );
-get_sidebar();
+// get_sidebar( 'content' );
+// get_sidebar();
 get_footer();
