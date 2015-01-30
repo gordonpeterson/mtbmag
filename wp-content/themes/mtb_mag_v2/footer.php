284affe54ed4e3ee9a4d01e280d402a199282db4
gordon peterson <gordon.peterson@imail.org>
2015-01-30 12:22:46 -0700

    added social media widget to the footer.

--- wp-content/themes/mtb_mag_v2/footer.php
+++ wp-content/themes/mtb_mag_v2/footer.php
@@ -14,12 +14,14 @@
 
 		<footer id="colophon" class="site-footer" role="contentinfo">
 
+				<?php 
+					if ( ! dynamic_sidebar( 'social-media' ) ) : 			
+					endif;
+				?>
+
 
 			<div class="site-info">
 				<?php do_action( 'twentyfourteen_credits' ); ?>
-				<!-- 
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyfourteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfourteen' ), 'WordPress' ); ?></a>
-				 -->
 			</div><!-- .site-info -->
 		</footer><!-- #colophon -->
 	</div><!-- #page -->
