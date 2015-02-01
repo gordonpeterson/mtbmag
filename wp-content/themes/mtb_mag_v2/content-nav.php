76b86ffe583afbd05b6e7112c5b12c3a8ec51e79
gordon peterson <gordon.peterson@imail.org>
2015-02-01 12:06:31 -0700

    cleaned up the article css to not overwrite nav css

--- wp-content/themes/mtb_mag_v2/content-nav.php
+++ wp-content/themes/mtb_mag_v2/content-nav.php
@@ -4,29 +4,17 @@
  */
 ?>
 
-<div id="post-<?php the_ID(); ?>" <?php post_class('nav'); ?>>
+<div id="post-<?php the_ID(); ?>" <?php post_class('nav-article'); ?>>
 	
 	<?php //twentyfourteen_post_thumbnail(); ?>
 	<div class="post-thumbnail">
-		<?php the_post_thumbnail('wt144_144'); ?>
+		<?php the_post_thumbnail('thumbnail'); ?>
 	</div>
 
 	<div class="post-info">
 		<header class="entry-header">
-			<?php if ( in_array( 'category', get_object_taxonomies( get_post_type() ) ) && twentyfourteen_categorized_blog() ) : ?>
-				<!-- 
-			<div class="entry-meta">
-				<span class="cat-links"><?php echo get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'twentyfourteen' ) ); ?></span>
-			</div>
-				 -->
 			<?php
-				endif;
-
-				if ( is_single() ) :
-					the_title( '<h3 class="entry-title">', '</h3>' );
-				else :
-					the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
-				endif;
+			the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
 			?>
 		</header><!-- .entry-header -->
 
